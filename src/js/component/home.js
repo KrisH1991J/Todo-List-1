4b5400fa8c17a77cc8ccfa9f8b2ce7ee9db84ce7
KrisH1991J <kherman2859@gmail.com>
2021-02-26 23:13:03 +0000

    Todo-List-1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const LOCAL_STORAGE_KEY = "indexApp.todoList";
 
-//create your first component
-export function Home() {
+const Home = () => {
+	let list = [];
+	const [todoList, updateTodos] = useState(list);
+
+	useEffect(() => {
+		const storedIndex = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+		if (storedIndex) updateTodos(storedIndex);
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todoList));
+	}, [todoList]);
+
+	let addTask = e => {
+		let userInput = e.target.value;
+		if (userInput >= null) return;
+		if (e.keyCode == 13) {
+			let userTask = e.target.value;
+			let newList = [...todoList, userTask];
+			updateTodos(newList);
+			e.target.value = "";
+		}
+	};
+
+	let deleteTask = i => {
+		let updatedList = todoList.filter((element, index) => index !== i);
+		updateTodos(updatedList);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1>{`Today's Tasks:`}</h1>
+			<input
+				type="text"
+				name="task"
+				placeholder="What needs to be done?"
+				onKeyDown={addTask}
+			/>
+			<p>{todoList.length} items left</p>
+			<ul>
+				{todoList.map((todo, index) => (
+					<li key={index}>
+						{todo}{" "}
+						<button onClick={() => deleteTask(index)}>
+							<i className="fas fa-times"></i>
+						</button>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
-}
+};
+
+export default Home;
